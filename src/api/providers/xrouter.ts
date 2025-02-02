cc1ada1d627b6dfb7506f80c743efc20f4c64048
olegische <to_snezhny@mail.ru>
2025-02-02 11:24:22 +0500

    refactor: update xrouter url

--- src/api/providers/xrouter.ts
+++ src/api/providers/xrouter.ts
@@ -8,8 +8,7 @@ import { ApiStream } from "../transform/stream"
 import delay from "delay"
 
 // Configuration
-export const xrouterBaseUrl = "http://localhost:8000/api/v1"
-// export const xrouterBaseUrl = "https://xrouter.ru/api/v1"
+export const xrouterBaseUrl = "http://xrouter.chat/api/v1"
 
 export class XRouterHandler implements ApiHandler {
     private options: ApiHandlerOptions
